62db830839b6aeb703566003eec9b7771e1a6fc1
Kyle Hu <lloydpearce4@gmail.com>
2025-01-04 15:19:40 -0800

    added basic pagination

--- src/components/ModalButtons/EditButton.tsx
+++ src/components/ModalButtons/EditButton.tsx
@@ -19,9 +19,6 @@ import { DatePicker } from "../DatePicker"
 // Icons
 import { TimePickerDemo } from "@/components/TimePicker/time-picker-demo"
 
-// Hook for Form States
-import useFormState from "@/hooks/useFormState"
-
 // Tanstack
 import { useMutation, useQueryClient } from "@tanstack/react-query"
 
@@ -31,42 +28,101 @@ import { Database } from "@/supabase/types"
 import { WorkLogFetch } from "@/types"
 
 // React
-import { useEffect } from "react"
 import { useSession } from "@/context/SessionContext"
+import { Controller, SubmitHandler, useForm } from "react-hook-form"
+import ErrorText from "./ErrorText"
+import { useState } from "react"
 
 type DataType = Database["public"]["Tables"]["work_logs"]["Row"]
+
+type Inputs = {
+    entryName: string
+    shiftStart: Date
+    shiftEnd: Date
+    shiftDate: Date
+    hourlyRate: number
+}
 
 interface Props {
     data: WorkLogFetch
 }
 
 export default function EditButton({ data }: Props) {
+    const {
+        register,
+        handleSubmit,
+        control,
+        reset,
+        setError,
+        formState: { errors },
+    } = useForm<Inputs>({
+        defaultValues: {
+            entryName: data.name,
+            hourlyRate: data.hourlyRate,
+            shiftDate: new Date(data.startTime),
+            shiftStart: new Date(data.startTime),
+            shiftEnd: new Date(data.endTime),
+        },
+    })
     const { session } = useSession()
-    const { formState, updateField } = useFormState()
 
     const queryClient = useQueryClient()
 
-    const submitEdit = async () => {
+    const [open, setOpen] = useState(false)
+
+    const submitEdit = async (formData: Inputs) => {
+        let hasError: boolean = false
+        if (formData.shiftStart > formData.shiftEnd) {
+            setError("shiftStart", {
+                type: "manual",
+                message: "Starting shift cannot be later than ending shift.",
+            })
+            hasError = true
+        }
+
+        if (formData.shiftStart.getTime() === formData.shiftEnd.getTime()) {
+            setError("shiftStart", {
+                type: "manual",
+                message: "Enter a valid shift.",
+            })
+            hasError = true
+        }
+
+        if (formData.shiftDate > new Date()) {
+            setError("shiftDate", {
+                type: "manual",
+                message: "Select a valid date.",
+            })
+            hasError = true
+        }
+
+        if (hasError) {
+            return []
+        }
+
+        // prettier-ignore
+        const {entryName, hourlyRate, shiftStart, shiftEnd, shiftDate} = formData
+
         // Build the data that will be inserted
-        const startTime = new Date(formState.shiftDate as Date)
-        const endTime = new Date(formState.shiftDate as Date)
+        const startTime = new Date(shiftDate as Date)
+        const endTime = new Date(shiftDate as Date)
 
         startTime.setHours(
-            formState.shiftStart.getHours(),
-            formState.shiftStart.getMinutes(),
-            formState.shiftStart.getSeconds(),
+            shiftStart.getHours(),
+            shiftStart.getMinutes(),
+            shiftStart.getSeconds(),
         )
 
         endTime.setHours(
-            formState.shiftEnd.getHours(),
-            formState.shiftEnd.getMinutes(),
-            formState.shiftEnd.getSeconds(),
+            shiftEnd.getHours(),
+            shiftEnd.getMinutes(),
+            shiftEnd.getSeconds(),
         )
 
         const updateData: DataType = {
             id: data.id,
-            hourly_rate: formState.hourlyRate,
-            name: formState.entryName,
+            hourly_rate: hourlyRate,
+            name: entryName,
             end_time: endTime.toISOString(),
             start_time: startTime.toISOString(),
             user_id: session?.user.id || "",
@@ -75,22 +131,20 @@ export default function EditButton({ data }: Props) {
         await editWorkLog(updateData)
 
         queryClient.invalidateQueries({ queryKey: ["workLogs"] })
+        reset()
+        setOpen(false)
     }
 
     const submitMutation = useMutation({
         mutationFn: submitEdit,
     })
 
-    useEffect(() => {
-        updateField("entryName", data.name)
-        updateField("hourlyRate", data.hourlyRate)
-        updateField("shiftDate", new Date(data.startTime))
-        updateField("shiftStart", new Date(data.startTime))
-        updateField("shiftEnd", new Date(data.endTime))
-    }, [])
+    const submitForm: SubmitHandler<Inputs> = (data) => {
+        submitMutation.mutate(data)
+    }
 
     return (
-        <Dialog>
+        <Dialog open={open} onOpenChange={setOpen}>
             <DialogTrigger asChild>
                 <p className="w-full">Edit</p>
             </DialogTrigger>
@@ -101,89 +155,128 @@ export default function EditButton({ data }: Props) {
                 onKeyDown={(e) => e.stopPropagation()}
                 onPointerMove={(e) => e.stopPropagation()}
             >
-                {/* Form Header */}
-                <DialogHeader>
-                    <DialogTitle className="text-white">
-                        Log your Hours
-                    </DialogTitle>
-                    <DialogDescription>
-                        Log your most recent work shift.
-                    </DialogDescription>
-                </DialogHeader>
+                <form
+                    onSubmit={handleSubmit(submitForm)}
+                    className="flex flex-col gap-7"
+                >
+                    {/* Form Header */}
+                    <DialogHeader>
+                        <DialogTitle className="text-white">
+                            Log your Hours
+                        </DialogTitle>
+                        <DialogDescription>
+                            Log your most recent work shift.
+                        </DialogDescription>
+                    </DialogHeader>
 
-                <div className="flex flex-col items-center gap-7 space-x-2">
-                    {/* Name of Entry */}
-                    <LabelInputContainer>
-                        <LabelTitle>Entry Name</LabelTitle>
-                        <Input
-                            className="text-white"
-                            type="text"
-                            placeholder="Entry Name"
-                            value={formState.entryName}
-                            onChange={(e) =>
-                                updateField("entryName", e.target.value)
-                            }
-                        />
-                    </LabelInputContainer>
+                    <div className="flex flex-col items-center gap-7 space-x-2">
+                        {/* Name of Entry */}
+                        <LabelInputContainer>
+                            <LabelTitle>Entry Name</LabelTitle>
+                            <Input
+                                className="text-white"
+                                type="text"
+                                placeholder="Entry Name"
+                                {...register("entryName", {
+                                    required: {
+                                        value: true,
+                                        message:
+                                            "Please provide a name for entry.",
+                                    },
+                                })}
+                            />
+                            {errors.entryName && (
+                                <ErrorText>
+                                    {errors.entryName.message}
+                                </ErrorText>
+                            )}
+                        </LabelInputContainer>
 
-                    {/* Hourly Rate */}
-                    <LabelInputContainer>
-                        <LabelTitle>Hourly Rate</LabelTitle>
-                        <Input
-                            type="number"
-                            placeholder="35"
-                            value={formState.hourlyRate}
-                            onChange={(e) =>
-                                updateField("hourlyRate", e.target.value)
-                            }
-                        />
-                    </LabelInputContainer>
+                        {/* Hourly Rate */}
+                        <LabelInputContainer>
+                            <LabelTitle>Hourly Rate</LabelTitle>
+                            <Input
+                                type="number"
+                                placeholder="35"
+                                {...register("hourlyRate")}
+                            />
+                        </LabelInputContainer>
 
-                    {/* Shift Start */}
-                    <LabelInputContainer>
-                        <LabelTitle>Shift Start</LabelTitle>
-                        <TimePickerDemo
-                            setDate={(date) => updateField("shiftStart", date)}
-                            date={formState.shiftStart}
-                        />
-                    </LabelInputContainer>
+                        {/* Shift Start */}
+                        <LabelInputContainer>
+                            <LabelTitle>Shift Start</LabelTitle>
+                            <Controller
+                                control={control}
+                                name="shiftStart"
+                                render={({ field: { onChange, value } }) => (
+                                    <TimePickerDemo
+                                        setDate={onChange}
+                                        date={value}
+                                    />
+                                )}
+                            />
+                            {errors.shiftStart && (
+                                <ErrorText>
+                                    {errors.shiftStart.message}
+                                </ErrorText>
+                            )}
+                        </LabelInputContainer>
 
-                    <LabelInputContainer>
-                        <LabelTitle>Shift End</LabelTitle>
-                        <TimePickerDemo
-                            setDate={(date) => updateField("shiftEnd", date)}
-                            date={formState.shiftEnd}
-                        />
-                    </LabelInputContainer>
+                        <LabelInputContainer>
+                            <LabelTitle>Shift End</LabelTitle>
+                            <Controller
+                                control={control}
+                                name="shiftEnd"
+                                rules={{
+                                    required: {
+                                        value: true,
+                                        message: "Please provide end of shift.",
+                                    },
+                                }}
+                                render={({ field: { onChange, value } }) => (
+                                    <TimePickerDemo
+                                        setDate={onChange}
+                                        date={value}
+                                    />
+                                )}
+                            />
+                            {errors.shiftEnd && (
+                                <ErrorText>{errors.shiftEnd.message}</ErrorText>
+                            )}
+                        </LabelInputContainer>
 
-                    {/* Date Picker */}
+                        {/* Date Picker */}
 
-                    <LabelInputContainer>
-                        <LabelTitle>Date of Shift</LabelTitle>
-                        <DatePicker
-                            formDate={formState.shiftDate}
-                            setFormDate={(date) =>
-                                updateField("shiftDate", date)
-                            }
-                        />
-                    </LabelInputContainer>
-                </div>
+                        <LabelInputContainer>
+                            <LabelTitle>Date of Shift</LabelTitle>
+                            <Controller
+                                control={control}
+                                name="shiftDate"
+                                render={({ field: { onChange, value } }) => (
+                                    <DatePicker
+                                        setFormDate={onChange}
+                                        formDate={value}
+                                    />
+                                )}
+                            />
+                            {errors.shiftDate && (
+                                <ErrorText>
+                                    {errors.shiftDate.message}
+                                </ErrorText>
+                            )}
+                        </LabelInputContainer>
+                    </div>
 
-                <DialogFooter className="flex flex-row justify-between">
-                    <DialogClose asChild>
-                        <Button type="button" variant="destructive">
-                            Cancel
-                        </Button>
-                    </DialogClose>
-                    <DialogClose asChild>
-                        <Button
-                            type="submit"
-                            onClick={() => submitMutation.mutate()}
-                        >
-                            Confirm
-                        </Button>
-                    </DialogClose>
-                </DialogFooter>
+                    <DialogFooter className="flex flex-row justify-between">
+                        <DialogClose asChild>
+                            <Button type="button" variant="destructive">
+                                Cancel
+                            </Button>
+                        </DialogClose>
+
+                        <Button type="submit">Confirm</Button>
+                    </DialogFooter>
+                </form>
             </DialogContent>
         </Dialog>
     )
